1dd4085c641a7cfcc74a20b3722a32a60f80b444
tangaoyang <809634953@qq.com>
2020-12-23 09:54:20 +0800

    Initial Commit

---
+++ detectiveTangTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  detectiveTangTest
+//
+//  Created by Cinderella on 2020/12/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ detectiveTangTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  detectiveTangTest
+//
+//  Created by Cinderella on 2020/12/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
